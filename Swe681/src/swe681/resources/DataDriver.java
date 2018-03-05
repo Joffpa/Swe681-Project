6c67654f2b469332fcb68d043fe5c11711aff16d
Joffrey Pannee <joffpa@gmail.com>
2018-03-05 18:37:35 -0500

    inital commit

---
+++ Swe681/src/swe681/resources/DataDriver.java
@@ -0,0 +1,49 @@
+package swe681.resources;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Vector;
+
+import javax.swing.table.DefaultTableModel;
+
+public class DataDriver {
+	static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+	static final String DB_URL = "jdbc:oracle:thin:@apollo.vse.gmu.edu:1521:ite10g";
+	static final String DB_USER = "jpannee";
+	static final String DB_PASS = "ystone";
+
+	private Connection conn = null;
+
+	public DataDriver() {
+		try {
+
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+		} catch (Exception ex) {
+			System.out.println("exception: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
+	public ResultSet getAirports() {
+		ResultSet rs = runQuery("select * from Airport");
+		return rs;
+	}
+	
+	private ResultSet runQuery(String query) {
+		ResultSet rs = null;
+		try {
+			PreparedStatement pStmt = conn.prepareStatement(query);
+			rs = pStmt.executeQuery();
+		} catch (Exception e) {
+		}
+		return rs;
+	}
+
+}
